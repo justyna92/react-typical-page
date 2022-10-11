fd9ac8c0696373dbc6260048923e58fa2ecb2726
justyna92 <justyna.binieda@gmail.com>
2022-10-11 19:06:41 +0200

    Tapping Into Context with the useContext Hook - correction

--- src/components/MainHeader/Navigation.js
+++ src/components/MainHeader/Navigation.js
@@ -7,33 +7,27 @@ const Navigation = (props) => {
   const context = useContext(AuthContext);
 
   return (
-    <AuthContext.Consumer>
-      {(context) => {
-        return (
-          <nav className={classes.nav}>
-            <ul>
-              {context.isLoggedIn && (
-                <li>
-                  <a href="/">Users</a>
-                </li>
-              )}
-              {context.isLoggedIn && (
-                <li>
-                  <a href="/">Admin</a>
-                </li>
-              )}
-              {context.isLoggedIn && (
-                <li>
-                  <button onClick={props.onLogout}>
-                    Logout {context.isLoggedIn}
-                  </button>
-                </li>
-              )}
-            </ul>
-          </nav>
-        );
-      }}
-    </AuthContext.Consumer>
+    <nav className={classes.nav}>
+      <ul>
+        {context.isLoggedIn && (
+          <li>
+            <a href="/">Users</a>
+          </li>
+        )}
+        {context.isLoggedIn && (
+          <li>
+            <a href="/">Admin</a>
+          </li>
+        )}
+        {context.isLoggedIn && (
+          <li>
+            <button onClick={props.onLogout}>
+              Logout {context.isLoggedIn}
+            </button>
+          </li>
+        )}
+      </ul>
+    </nav>
   );
 };
 
